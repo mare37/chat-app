c6f5a1c81d98a9eb550c15ab576a27753a93195c
Wilson Mare <marewilson35@gmail.com>
2022-12-12 11:41:20 +0200

    deleted chat

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Chat from "./Components/Chat/chat";
+
 import Home from "./Components/Home/home";
 import LogIn from "./Components/Login/login";
 
